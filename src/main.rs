c3ffbe63c535605b7cda91b590a04d4c0c3e44cb
Emil Hultcrantz <emilhtz@gmail.com>
2025-05-23 13:16:28 +0200

    Add parsing from parse-tree to ast

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-use compiler::parser::parse_output;
+use compiler::parser::parse_program;
 
 mod compiler;
 
-use std::io::{stdin, Read};
+use std::io::{Read, stdin};
 
 fn main() {
     let mut file = String::new();
     stdin().lock().read_to_string(&mut file).unwrap();
-    parse_output(&file).unwrap();
+    match parse_program(&file) {
+        Err(err) => eprintln!("{:#}", err),
+        Ok(ast) => {
+            for stmt in ast.0 {
+                println!("{:?}", stmt);
+            }
+        }
+    }
 }
